5a5a18052c1a74cc598ba3cb2389605f4b392eaf
kartikeya47 <kartikeya.rawat2020@vitstudent.ac.in>
2022-11-06 13:01:15 +0530

    First Commit

---
+++ app/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect
+import os
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+    if request.method == "POST":
+        
+        nature = request.form.get('nature')
+        fight = request.form.get('fight')
+        cook = request.form.get('cook')
+        clean = request.form.get('clean')
+        model = pickle.load(open('app\model', 'rb'))
+        
+        li = []
+        
+        if nature == 'Good':
+            li.append(1)
+            li.append(0)        
+        if nature == 'Average':
+            li.append(0)
+            li.append(0)    
+        if nature == 'Worst':
+            li.append(0)
+            li.append(1)
+            
+        if fight == 'Fights':
+            li.append(0)
+        if fight == 'Not Fights':
+            li.append(1)
+            
+        if cook == 'No':
+            li.append(0)
+        if cook == 'Yes':
+            li.append(1)
+            
+        if clean == 'No':
+            li.append(0)
+        if clean == 'Yes':
+            li.append(1)
+        
+        listt = [li]
+        result = model.predict(listt)
+    
+    return render_template('output.html', result=result, listt=listt)
